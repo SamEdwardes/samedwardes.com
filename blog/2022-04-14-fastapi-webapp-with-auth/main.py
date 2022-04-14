242380f975cc341abd5b0cdec5d30a504bd4c1d6
SamEdwardes <edwardes.s@gmail.com>
2022-04-14 15:49:29 -0700

    Token saved in Cookie!!

--- blog/2022-04-14-fastapi-webapp-with-auth/main.py
+++ blog/2022-04-14-fastapi-webapp-with-auth/main.py
@@ -1,10 +1,12 @@
 from distutils.log import Log
 from typing import Optional, List
+import datetime as dt
+from jose import jwt
 
-from fastapi import FastAPI, Request, Form, Depends
+from fastapi import FastAPI, Request, Form, Depends, Response, HTTPException, status
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from fastapi_login.exceptions import InvalidCredentialsException
 from fastapi.templating import Jinja2Templates
 from fastapi_login import LoginManager
@@ -19,8 +21,10 @@ console = Console()
 # Models
 # --------------------------------------------------------------------------
 class User(BaseModel):
-    email: str
+    username: str
     password: str
+
+
 
 
 # --------------------------------------------------------------------------
@@ -33,8 +37,8 @@ class DataBase(BaseModel):
 
 DB = DataBase(
     user=[
-        User(email="user1@gmail.com", password="12345"),
-        User(email="user2@gmail.com", password="12345"),
+        User(username="user1@gmail.com", password="12345"),
+        User(username="user2@gmail.com", password="12345"),
     ]
 )
 
@@ -43,41 +47,84 @@ DB = DataBase(
 # --------------------------------------------------------------------------
 # Setup FastAPI
 # --------------------------------------------------------------------------
+class Settings:
+    SECRET_KEY: str = "secret-key"
+    ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30  # in mins
+
+
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
-SECRECT = "7c33ead4881bb004dc2959518f2cf7dd7bdb28a26be7009b"
-manager =  LoginManager(SECRECT, "/login/get_token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token")
+settings = Settings()
 
 
-@manager.user_loader()
-def query_user(email: str) -> User:
-    user = [user for user in DB.user if user.email == email]
+# --------------------------------------------------------------------------
+# Authentication logic
+# --------------------------------------------------------------------------
+def create_access_token(
+    data: dict, 
+    expires_delta: Optional[dt.timedelta] = None
+):
+    console.log("create_access_token()")
+    to_encode = data.copy()
+    if expires_delta:
+        expire = dt.datetime.utcnow() + expires_delta
+    else:
+        expire = dt.datetime.utcnow() + dt.timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+    )
+    return encoded_jwt
+
+
+def get_user(username: str) -> User:
+    user = [user for user in DB.user if user.username == username]
     if user:
         return user[0]
     else:
         return None
 
-# @app.on_event("startup")
-# async def app_init():
-#     client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
-#     database_names = await client.list_database_names()
-    
 
-    
-#     if create_demo_data:
-#         print("Creating demo data...")
-#         await create_data()
+def authenticate_user(username: str, password: str) -> User:
+    user = get_user(username)
+    console.log(locals())
+    # (1) Validate if the user exists.
+    if not user:
+        return False
+    # (2) Validate if the
+    # TODO: hash the password
+    elif password != user.password:
+        return False
+    else:
+        return user
 
+
+@app.post("/auth/token")
+def login_for_access_token(
+    response: Response, 
+    form_data: OAuth2PasswordRequestForm = Depends()
+):
+    console.log("login_for_access_token()")
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password",)
+    access_token_expires = dt.timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    response.set_cookie(key="access_token",value=f"Bearer {access_token}", httponly=True)  #set HttpOnly cookie in response
+    return {"access_token": access_token, "token_type": "bearer"}
 
 # --------------------------------------------------------------------------
 # Home Page
 # --------------------------------------------------------------------------
 @app.get("/", response_class=HTMLResponse)
-def index(request: Request, user: User = Depends(manager)):
+def index(request: Request):
     console.rule(f"{request.method} ~ {request.url.path}")
     context = {
         "request": request,
-        "user": user,
     }
     return templates.TemplateResponse("index.html", context)
 
@@ -85,8 +132,8 @@ def index(request: Request, user: User = Depends(manager)):
 # --------------------------------------------------------------------------
 # Authentication
 # --------------------------------------------------------------------------
-@app.get("/login", response_class=HTMLResponse)
-def login(request: Request):
+@app.get("/auth/login", response_class=HTMLResponse)
+def login_get(request: Request):
     console.rule(f"{request.method} ~ {request.url.path}")
     context = {
         "request": request,
@@ -104,9 +151,7 @@ class LoginForm:
 
     async def load_data(self):
         form = await self.request.form()
-        self.username = form.get(
-            "email"
-        )  # since outh works on username field we are considering email as username
+        self.username = form.get("username")  # since outh works on username field we are considering email as username
         self.password = form.get("password")
 
     async def is_valid(self):
@@ -119,43 +164,47 @@ class LoginForm:
         return False
 
 
-@app.post("/login", response_class=HTMLResponse)
-async def login(request: Request):
+@app.post("/auth/login", response_class=HTMLResponse)
+async def login_post(request: Request):
     console.rule(f"{request.method} ~ {request.url.path}")
     form = LoginForm(request)
     await form.load_data()
+    console.log(form.__dict__)
     if await form.is_valid():
-        
+        try:
+            form.__dict__.update(msg="Login Successful!")
+            response = templates.TemplateResponse("login.html", form.__dict__)
+            login_for_access_token(response=response, form_data=form)
+            console.log("[green]Login successful!!!!")
+            return response
+        except HTTPException:
+            form.__dict__.update(msg="")
+            form.__dict__.get("errors").append("Incorrect Email or Password")
+            return templates.TemplateResponse("login.html", form.__dict__)
+    return templates.TemplateResponse("login.html", form.__dict__)
 
 
-    context = {
-        "request": request,
-        "user": None,
-    }
-    return templates.TemplateResponse("login.html", context)
 
 
+# @app.post('/auth/token')
+# def login(request: Request, data: OAuth2PasswordRequestForm = Depends()):
+#     console.rule(f"{request.method} ~ {request.url.path}")
 
+#     email = data.username
+#     password = data.password
+#     user = query_user(email)
+#     inspect(user, title="user")
 
-@app.post('/login/get_token')
-def login(request: Request, data: OAuth2PasswordRequestForm = Depends()):
-    console.rule(f"{request.method} ~ {request.url.path}")
-
-    email = data.username
-    password = data.password
-    user = query_user(email)
-    inspect(user, title="user")
-
-    if not user:
-        console.log(f"[red bold]User {email} not found")
-        raise InvalidCredentialsException
-    elif password != user.password:
-        console.log(f"[red bold]Password does not match!")
-        raise InvalidCredentialsException
-    else:
-        console.log("[green] Auth successful!")
-    access_token = manager.create_access_token(
-        data={'sub': email}
-    )
-    return {'access_token': access_token, "token_type": "bearer"}
+#     if not user:
+#         console.log(f"[red bold]User {email} not found")
+#         raise InvalidCredentialsException
+#     elif password != user.password:
+#         console.log(f"[red bold]Password does not match!")
+#         raise InvalidCredentialsException
+#     else:
+#         console.log("[green] Auth successful!")
+#     access_token = manager.create_access_token(
+#         data={'sub': email}
+#     )
+#     return {'access_token': access_token, "token_type": "bearer"}
 
