9fa0ab32818be05ac31a4d9611cccaff0d3ab414
SamEdwardes <edwardes.s@gmail.com>
2024-05-26 15:24:28 -0700

    Remove starlight

--- astro-migration/astro.config.mjs
+++ astro-migration/astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
-    starlight({
-      title: "samedwardes.com",
-    })
   ],
 });
