d56875060dd8452935b55ba33ebd842c23999b24
SamEdwardes <edwardes.s@gmail.com>
2024-05-26 18:54:13 -0700

    Add alpine.js integration

--- astro-migration/astro.config.mjs
+++ astro-migration/astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx(), alpinejs()]
 });
