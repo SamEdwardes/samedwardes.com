021ebe15788740999ff94543a47a89687e6c0535
SamEdwardes <edwardes.s@gmail.com>
2024-08-27 16:38:29 -0700

    Format and add RSS icon/link to footer

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,12 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context) {
-  const blog = await getCollection("blog")
+  const blog = await getCollection("blog");
   return rss({
-    title: 'samedwardes.com Blog',
-    description: 'Sam Edwardes personal blog about Python, R, datascience, the web, and a few other random things.',
+    title: "samedwardes.com Blog",
+    description:
+      "Sam Edwardes personal blog about Python, R, datascience, the web, and a few other random things.",
     site: context.site,
     items: blog.map((post) => ({
       title: post.data.title,
