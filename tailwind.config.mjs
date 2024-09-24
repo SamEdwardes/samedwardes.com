8246154fa2632733af6940a27248c9f4886f1023
SamEdwardes <edwardes.s@gmail.com>
2024-09-23 20:13:36 -0700

    Use Atkinson Hyperlegible font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        'sans': ["Atkinson Hyperlegible", ...defaultTheme.fontFamily.sans]
+      },
       colors: {
         primary: "#3c7dc1",
       },
