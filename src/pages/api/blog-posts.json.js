6309bba67f5b41b2a7cde8ac320683b958dbde65
SamEdwardes <edwardes.s@gmail.com>
2025-03-11 10:24:11 -0700

    Basic search that returns results is working

--- src/pages/api/blog-posts.json.js
+++ src/pages/api/blog-posts.json.js
@@ -1,15 +1,19 @@
 import { getCollection } from "astro:content";
 
 async function getPosts() {
-  const posts = (await getCollection("blog")).sort(
-    (a, b) => a.data.date.valueOf() - b.data.date.valueOf()
-  );
+  const posts = (
+    await getCollection("blog", ({ data }) => {
+      return data.draft !== true;
+    })
+  ).sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf());
 
   // return posts;
   return posts.map((post) => ({
     slug: post.slug,
     title: post.data.title,
     description: post.data.description,
+    tags: post.data.tags,
+    keywords: post.data.keywords,
     date: post.data.pubDate,
   }));
 }
